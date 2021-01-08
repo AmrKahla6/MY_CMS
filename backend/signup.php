31a3d7ea07db135059844b8f3d42b22c0f5bb5de
AmrKahla6 <amrkahla6@gmail.com>
2021-01-08 18:18:55 +0200

    signup page

--- backend/signup.php
+++ backend/signup.php
@@ -1,3 +1,4 @@
+<?php include_once "../inc/db.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,35 +15,85 @@
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
+                <?php
+                    if(isset($_POST['submit']))
+                    {
+                        $first_name = trim($_POST['first-name']);
+                        $last_name  = trim($_POST['last-name']);
+                        $full_name  = $first_name .' '. $last_name;
+                        $email      = trim($_POST['email']);
+                        $password   = trim($_POST['password']);
+                        $conf_pass  = trim($_POST['confirm-password']);
+
+                        if($password != $conf_pass)
+                        {
+                            $error = "Password doesn't match";
+                        }
+                        else
+                        {
+                            $sql = "INSERT INTO users (user_name , user_email, user_password, user_photo, registered_on)
+                                    VALUE (:name, :email, :password, :photo, :data)";
+
+                            $stmt = $pdo->prepare($sql);
+                            $stmt->execute([
+                                ':name'     => $full_name,
+                                ':email'    => $email,
+                                ':password' => $password,
+                                ':photo'   => 'default.png',
+                                ':data'     => "9 Jun 2021"
+                            ]);
+                        }
+                    }
+                ?>
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-7">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header justify-content-center"><h3 class="font-weight-light my-4">Create Account</h3></div>
                                     <div class="card-body">
-                                        <form>
+                                        <form action="signup.php" method="post">
+                                            <?php
+                                                if(isset($error))
+                                                {
+                                                    echo "<div class='alert alert-danger text-center'>{$error}</div><hr>";
+                                                }
+                                            ?>
                                             <div class="form-row">
                                                 <div class="col-md-6">
-                                                    <div class="form-group"><label class="small mb-1" for="inputFirstName">First Name</label><input class="form-control py-4" id="inputFirstName" type="text" placeholder="Enter first name" /></div>
+                                                    <div class="form-group"><label class="small mb-1" for="inputFirstName">First Name</label>
+                                                         <input name="first-name" required class="form-control py-4" id="inputFirstName" type="text" placeholder="Enter first name"/>
+                                                    </div>
                                                 </div>
                                                 <div class="col-md-6">
-                                                    <div class="form-group"><label class="small mb-1" for="inputLastName">Last Name</label><input class="form-control py-4" id="inputLastName" type="text" placeholder="Enter last name" /></div>
+                                                    <div class="form-group"><label class="small mb-1" for="inputLastName">Last Name</label>
+                                                        <input name="last-name" required class="form-control py-4" id="inputLastName" type="text" placeholder="Enter last name" />
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group"><label class="small mb-1" for="inputEmailAddress">Email</label><input class="form-control py-4" id="inputEmailAddress" type="email" aria-describedby="emailHelp" placeholder="Enter email address" /></div>
+                                            <div class="form-group"><label class="small mb-1" for="inputEmailAddress">Email</label>
+                                                <input name="email"  required class="form-control py-4" id="inputEmailAddress" type="email" aria-describedby="emailHelp" placeholder="Enter email address" />
+                                            </div>
                                             <div class="form-row">
                                                 <div class="col-md-6">
-                                                    <div class="form-group"><label class="small mb-1" for="inputPassword">Password</label><input class="form-control py-4" id="inputPassword" type="password" placeholder="Enter password" /></div>
+                                                    <div class="form-group"><label class="small mb-1" for="inputPassword">Password</label>
+                                                        <input name="password" required class="form-control py-4" id="inputPassword" type="password" placeholder="Enter password" />
+                                                    </div>
                                                 </div>
                                                 <div class="col-md-6">
-                                                    <div class="form-group"><label class="small mb-1" for="inputConfirmPassword">Confirm Password</label><input class="form-control py-4" id="inputConfirmPassword" type="password" placeholder="Confirm password" /></div>
+                                                    <div class="form-group"><label class="small mb-1" for="inputConfirmPassword">Confirm Password</label>
+                                                         <input name="confirm-password" required class="form-control py-4" id="inputConfirmPassword" type="password" placeholder="Confirm password" />
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group mt-4 mb-0"><a class="btn btn-primary btn-block" href="#">Create Account</a></div>
+                                            <div class="form-group mt-4 mb-0">
+                                                <button name="submit" class="btn btn-primary btn-block">Create Account</button>
+                                            </div>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center">
-                                        <div class="small"><a href="signin.html">Have an account? Go to signin</a></div>
+                                        <div class="small">
+                                            <a href="signin.php">Have an account? Go to signin</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
