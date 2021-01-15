5f6b8e510a5fe0a38890f0b0ce88d009dce9c1ad
AmrKahla6 <amrkahla6@gmail.com>
2021-01-15 17:43:18 +0200

    Add new category

--- backend/new-category.php
+++ backend/new-category.php
@@ -29,24 +29,62 @@
                         </div>
                     </div>
 
+                    <?php
+                        if(isset($_POST['button'])){
+                            $category_name   = trim($_POST['category_name']);
+                            $category_status = $_POST['category_status'];
+
+                            if(isset($_COOKIE['_uid_'])){
+                                $user_id = base64_decode($_COOKIE['_uid_']);
+
+                                $sql  = "SELECT * FROM users WHERE user_id = :id";
+                                $stmt = $pdo->prepare($sql);
+                                $stmt->execute([
+                                    ':id' => $user_id
+                                ]);
+                                $user       = $stmt->fetch(PDO::FETCH_ASSOC);
+                                $user_name  = $user['user_name'];
+
+                            } elseif($_SESSION['user_name']){
+                                $user_name = $_SESSION['user_name'];
+                            }
+
+                            $sql  = "INSERT INTO categories (category_name, category_total_posts, total_post_views, category_status, created_on, created_by)
+                                     VALUES (:cat_name, :total_post, :total_views, :cat_status, :created_on, :created_by )";
+
+                            $stmt = $pdo->prepare($sql);
+                            $stmt->execute([
+                                ':cat_name'     => $category_name,
+                                ':total_post'   => 0,
+                                ':total_views'  => 0,
+                                ':cat_status'   => $category_status,
+                                ':created_on'   => date("M n, Y") . ' at ' . date("h:i A"),
+                                ':created_by'   => $user_name
+                            ]);
+
+                            header("Location: categories.php");
+                        }
+                    ?>
+
                     <!--Start Table-->
                     <div class="container-fluid mt-n10">
                         <div class="card mb-4">
                             <div class="card-header">Create New Category</div>
                             <div class="card-body">
-                                <form>
+                                <form action="new-category.php" method="post">
                                     <div class="form-group">
                                         <label for="post-title">Category Name:</label>
-                                        <input class="form-control" id="post-title" type="text" placeholder="Category Name..." />
+                                        <input class="form-control" name="category_name"  value="<?php echo isset($_POST['category_name'])?$_POST['category_name']:'' ?>"
+                                                id="post-title" type="text" placeholder="Category Name..." required/>
                                     </div>
                                     <div class="form-group">
                                         <label for="post-status">Status:</label>
-                                        <select class="form-control" id="post-status">
-                                            <option>Published</option>
-                                            <option>Draft</option>
+                                        <select class="form-control" name="category_status" id="post-status">
+                                            <option value="published" <?php if(isset($_POST["category_status"]) && $_POST["category_status"] == 'published') { echo 'selected' ;} ?>>Published</option>
+                                            <option value="pendding"  <?php if(isset($_POST["category_status"]) && $_POST["category_status"] == 'pendding') { echo 'selected' ;} ?>>Pendding</option>
                                         </select>
                                     </div>
-                                    <button class="btn btn-primary mr-2 my-1" type="button">Submit now</button>
+                                    <button name="button" type="submit" class="btn btn-primary mr-2 my-1">Submit now</button>
                                 </form>
                             </div>
                         </div>
