246bcb1592af6039a84a0dcc0694801da64ccb82
AmrKahla6 <amrkahla6@gmail.com>
2021-01-15 16:26:33 +0200

    Category Dashboard

--- backend/categories.php
+++ backend/categories.php
@@ -41,10 +41,10 @@
                                     <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
                                         <thead>
                                             <tr>
+                                                <th>#</th>
                                                 <th>ID</th>
                                                 <th>Category Name</th>
                                                 <th>Total Posts</th>
-                                                <th>Post Views</th>
                                                 <th>Created By</th>
                                                 <th>Status</th>
                                                 <th>Edit</th>
@@ -52,69 +52,48 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>
-                                                    <a href="#">
-                                                        Lifestyle
-                                                    </a>
-                                                </td>
-                                                <td>20</td>
-                                                <td>61</td>
-                                                <td>Md. A. Barik</td>
-                                                <td>
-                                                    <div class="badge badge-success">Published
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <button class="btn btn-blue btn-icon"><i data-feather="edit"></i></button>
-                                                </td>
-                                                <td>
-                                                    <button class="btn btn-red btn-icon"><i data-feather="trash-2"></i></button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>2</td>
-                                                <td>
-                                                    <a href="#">
-                                                        Loved
-                                                    </a>
-                                                </td>
-                                                <td>20</td>
-                                                <td>61</td>
-                                                <td>Md. A. Barik</td>
-                                                <td>
-                                                    <div class="badge badge-success">Published
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <button class="btn btn-blue btn-icon"><i data-feather="edit"></i></button>
-                                                </td>
-                                                <td>
-                                                    <button class="btn btn-red btn-icon"><i data-feather="trash-2"></i></button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</td>
-                                                <td>
-                                                    <a href="#">
-                                                        Programming
-                                                    </a>
-                                                </td>
-                                                <td>20</td>
-                                                <td>61</td>
-                                                <td>Md. A. Barik</td>
-                                                <td>
-                                                    <div class="badge badge-success">Published
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <button class="btn btn-blue btn-icon"><i data-feather="edit"></i></button>
-                                                </td>
-                                                <td>
-                                                    <button class="btn btn-red btn-icon"><i data-feather="trash-2"></i></button>
-                                                </td>
-                                            </tr>
+                                        <?php
+                                            $sql  = "SELECT * FROM categories ORDER BY category_total_posts DESC";
+                                            $stmt = $pdo->prepare($sql);
+                                            $stmt->execute();
+                                            $x = 1;
+                                            while($categoris = $stmt->fetch(PDO::FETCH_ASSOC)){
+                                                $category_id     = $categoris['category_id'];
+                                                $category_name   = $categoris['category_name'];
+                                                $total_posts     = $categoris['category_total_posts'];
+                                                $category_status = $categoris['category_status'];
+                                                $created_by      = $categoris['created_by']; ?>
+
+                                                    <tr>
+                                                        <td><?php echo $x++ ?></td>
+                                                        <td><?php echo $category_id ?></td>
+                                                        <td>
+                                                            <a href="../categories.php?category_id=<?php echo $category_id ?>&category_name=<?php echo $category_name ?>" target="_blank">
+                                                                <?php echo $category_name ?>
+                                                            </a>
+                                                        </td>
+                                                        <td><?php echo $total_posts ?></td>
+                                                        <td><?php echo $created_by ?></td>
+                                                        <td>
+                                                            <?php
+                                                                if($category_status == "published"){?>
+                                                                    <div class="badge badge-success"><?php echo $category_status ?>
+                                                                <?php } else { ?>
+                                                                    <div class="badge badge-warning"><?php echo $category_status ?>
+                                                               <?php }
+                                                            ?>
+                                                            </div>
+                                                        </td>
+                                                        <td>
+                                                            <button class="btn btn-blue btn-icon"><i data-feather="edit"></i></button>
+                                                        </td>
+                                                        <td>
+                                                            <button class="btn btn-red btn-icon"><i data-feather="trash-2"></i></button>
+                                                        </td>
+                                                    </tr>
+
+                                            <?php }
+                                        ?>
                                         </tbody>
                                     </table>
                                 </div>
