014a91d004695467d6e10c55f698c82890d008ca
AmrKahla6 <amrkahla6@gmail.com>
2021-01-16 20:26:43 +0200

    Sidebar user information

--- backend/inc/navbar.php
+++ backend/inc/navbar.php
@@ -78,29 +78,52 @@
                     </div>
                 </li>
                 <!--Message Notification-->
+                <?php
+                    if(isset($_COOKIE['_uid_'])){
+                        $userid = base64_decode($_COOKIE['_uid_']);
+                    }else if(isset($_SESSION['user_id'])){
+                        $userid = $_SESSION['user_id'];
+                    } else {
+                        $userid = -1;
+                    }
+
+                    $sql  = "SELECT user_name, user_email, user_photo FROM users WHERE user_id = :id";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute([
+                        ':id' => $userid,
+                    ]);
+
+                    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                    $username  = $user['user_name'];
+                    $useremail = $user['user_email'];
+                    $userphoto = $user['user_photo']
+                ?>
 
                 <li class="nav-item dropdown no-caret mr-3 dropdown-user">
-                    <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="./assets/img/mdabarik.jpg"/></a>
+                    <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img class="img-fluid" src="../img/<?php echo $userphoto ?>"/>
+                    </a>
                     <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
                         <h6 class="dropdown-header d-flex align-items-center">
-                            <img class="dropdown-user-img" src="./assets/img/mdabarik.jpg" alt="Photo" />
+                            <img class="dropdown-user-img" src="../img/<?php echo $userphoto ?>" alt="<?php echo $username ?>" />
                             <div class="dropdown-user-details">
                                 <div class="dropdown-user-details-name">
-                                    Md. A. Barik
+                                    <?php echo $username ?>
                                 </div>
                                 <div class="dropdown-user-details-email">
-                                    mdabarik@gmail.com
+                                <?php echo $useremail ?>
                                 </div>
                             </div>
                         </h6>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="profile.html">
+                        <a class="dropdown-item" href="profile.php">
                             <div class="dropdown-item-icon">
                                 <i data-feather="settings"></i>
                             </div>
                             Profile
                         </a>
-                        <a class="dropdown-item" href="#"
+                        <a class="dropdown-item" href="logout.php"
                             ><div class="dropdown-item-icon">
                                 <i data-feather="log-out"></i>
                             </div>
